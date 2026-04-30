305573de2c62302acee7a59a7ab429c1db3f30bf
jlau4-gif <jlau4@emich.edu>
2026-04-30 09:21:09 -0400

    Create DatabaseConn class for database connection
    This class establishes a connection to the database with specified parameters and error handling.

---
+++ Final Project/classes/Db_conn.php
@@ -0,0 +1,32 @@
+<?php
+
+class DatabaseConn {	
+
+  private $conn;
+  /* THIS CLASS CONNECTS TO THE DATABASE ONLY AND SETS UP THE ATTRIBUTE PARAMETERS */
+  public function dbOpen(){
+
+    try {
+
+      $dbHost = 'localhost';
+      $dbName = 'jalau'; //this will be your WCC username
+      $dbUsr = 'jalau';  //this will be your WCC username
+      $dbPass = 'UtSrmK5NDj9zTXe';
+
+      $this->conn = new PDO('mysql:host=' . $dbHost . ';dbname=' . $dbName, $dbUsr, $dbPass);
+      $this->conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); /*THIS STOPS PDO FROM ADDING SINGLE QUOTES AROUND INTEGER VALUES.*/
+      $this->conn->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);/* FORCES QUERIES TO BE BUFFERED IN MYSQL */
+      $this->conn->setAttribute(PDO::ATTR_AUTOCOMMIT, true);
+      $this->conn->setAttribute(PDO::MYSQL_ATTR_LOCAL_INFILE, true);
+      $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+      return $this->conn;
+
+    }
+      
+    catch(PDOException $e) { 
+      throw $e;
+    }
+
+  }
+}
